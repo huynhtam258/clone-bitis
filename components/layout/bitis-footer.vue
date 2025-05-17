21bde3a7cad30e95734741fffd35149767459782
HuynhManhTam <huynhtam258@gmail.com>
2025-05-17 08:37:40 +0700

    feat: responsive license

--- components/layout/bitis-footer.vue
+++ components/layout/bitis-footer.vue
@@ -9,31 +9,7 @@
           <bitis-contract />
         </div>
       </section>
-      <section class="bitis-footer__bottom">
-        <div class="container content-bottom d-flex">
-          <div class="copyright content-bottom__item">
-            <div class="d-flex flex-wrap policy-container">
-              <div class="policy-item">Điều khoản</div>
-              <div class="policy-item">Chính sách bảo mật</div>
-              <div class="policy-item">Hướng dẫn sử dụng</div>
-            </div>
-            <p>
-              Copyright © 2025 Biti's.
-              <a href="https://www.haravan.com/" target="_blank">
-                Powered by Haravan Enterprise
-              </a>
-            </p>
-          </div>
-          <div class="content-bottom__item approved-icon">
-            <NuxtImg src="/logos/bocongthuong_confirm_medium.png" :width="120" :height="45" alt="Bộ công thương"
-              format="webp" />
-          </div>
-          <div class="certificate content-bottom__item">
-            Giấy CNĐKDN: 0301340497 được cấp ngày 20/01/1992, được sửa đổi lần thứ 25 ngày 27/01/2022 bởi Sở Kế hoạch và
-            Đầu tư TPHCM
-          </div>
-        </div>
-      </section>
+      <bitis-license />
     </section>
 
     <section class="mobile footer-mobile">
@@ -67,6 +43,7 @@
               <bitis-contract />
             </template>
           </bitis-expansion>
+          <bitis-license />
         </template>
       </bitis-expansion>
     </section>
@@ -79,7 +56,7 @@ import BitisGallery from './bitis-gallery.vue';
 import BitisNewsletter from './bitis-newsletter.vue';
 import BitisInfoSection from './bitis-info-section.vue';
 import BitisContract from './bitis-contract.vue';
-
+import BitisLicense from "./bitis-license.vue";
 const aboutBitis = {
   title: "Về Biti's",
   menuItems: [
@@ -133,40 +110,8 @@ const menuOptionList = [
     }
   }
 
-  &__bottom {
-    background-color: #dfdfdf;
-    border-top: 1px solid #e7e7e7;
-    color: #333333;
-    padding: 15px 0;
-
-    .content-bottom {
-      align-items: center;
-      justify-content: space-between;
-
-      &.container {
-        margin: auto;
-      }
-
-      .copyright {
-        .policy-container {
-          gap: 20px;
-
-          .policy-item {
-            list-style: none;
-          }
-        }
-      }
-
-      &__item {
-        width: 30%;
-
-        &.approved-icon {
-          text-align: center;
-        }
-      }
-    }
-
-  }
+  // &__bottom {
+  // }
 }
 
 .footer-mobile {
---
+++ components/layout/bitis-license.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="bitis-footer__bottom bitis-license">
+    <div class="container content-bottom d-flex">
+      <div class="copyright content-bottom__item">
+        <div class="d-flex flex-wrap policy-container">
+          <div class="policy-item">Điều khoản</div>
+          <div class="policy-item">Chính sách bảo mật</div>
+          <div class="policy-item">Hướng dẫn sử dụng</div>
+        </div>
+        <p class="powered">
+          <span>Copyright © 2025 Biti's.</span>
+          <a href="https://www.haravan.com/" target="_blank">
+            Powered by Haravan Enterprise
+          </a>
+        </p>
+      </div>
+      <div class="content-bottom__item approved-icon">
+        <NuxtImg src="/logos/bocongthuong_confirm_medium.png" :width="120" :height="45" alt="Bộ công thương"
+          format="webp" />
+      </div>
+      <div class="certificate content-bottom__item">
+        Giấy CNĐKDN: 0301340497 được cấp ngày 20/01/1992, được sửa đổi lần thứ 25 ngày 27/01/2022 bởi Sở Kế hoạch và
+        Đầu tư TPHCM
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.bitis-license {
+  background-color: #dfdfdf;
+  border-top: 1px solid #e7e7e7;
+  color: #333333;
+  padding: 15px 0;
+
+  .content-bottom {
+    align-items: center;
+    justify-content: space-between;
+
+    &.container {
+      margin: auto;
+    }
+
+    .copyright {
+      .policy-container {
+        gap: 20px;
+
+        .policy-item {
+          list-style: none;
+        }
+      }
+    }
+
+    &__item {
+      width: 30%;
+
+      &.approved-icon {
+        text-align: center;
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  .bitis-license {
+    padding: 15px ;
+    font-weight: 500;
+    .content-bottom {
+      flex-direction: column-reverse;
+      gap: 15px;
+      &__item {
+        width: 100%;
+        text-align: left;
+        line-height: 22.4px;
+        .policy-container {
+          gap: 10px !important;
+          justify-content: center;
+        }
+        .powered {
+          margin: 15px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+      }
+    }
+  }
+}
+</style>
